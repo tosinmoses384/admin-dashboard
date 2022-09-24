b5c60307aa72d4eee46cd16fbaf041e8580462e4
Israel <tosinmoses384@gmail.com>
2022-09-24 15:33:44 +0100

    feat(sidebar): - the following were added:   - build screen for the sidebar menu.   - display links for sidebar menu.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Router, Route } from 'react-router-dom';
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import { Ecommerce } from "./pages";
 import "./App.css";
 
 const App = () => {
+  const activeMenu  = true;
+  // const activeMenu = false;
   return (
     <div>
       <BrowserRouter>
-      <div className='flex relative dark:bg-main-dark-bg'>
-        <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
-          <TooltipComponent content='Settings' position='Top'>
-            <button>
-              <FiSettings />
-            </button>
-          </TooltipComponent>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {/* SIDE BAR */}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          {/* NAVIGATION MENU */}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+          </div>
+          {/* ROUTING CONTAINER */}
+          <div>
+            <Routes>
+              {/* DASHBOARD */}
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+            </Routes>
+          </div>
         </div>
-      </div>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { links } from "../data/dummy";
+
+const Sidebar = () => {
+  const handleCloseSideBar = () => {
+    console.log("handle click function");
+
+    // if (activeMenu !== undefined && screenSize <= 900) {
+    //   setActiveMenu(false);
+    // }
+  };
+
+  const activeMenu = true;
+
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            {/* APP LOGO */}
+            <Link
+              to="/"
+              onClick={handleCloseSideBar}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <SiShopware /> <span>Shoppy</span>
+            </Link>
+            {/* CLOSE SIDEBAR */}
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => console.log("set active menu")}
+                // onClick={() => setActiveMenu(!activeMenu)}
+
+                // style={{ color: currentColor }}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className="mt-10 ">
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={handleCloseSideBar}
+
+                    // style={({ isActive }) => ({
+                    //   backgroundColor: isActive ? currentColor : "",
+                    // })}
+
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    {link.icon}
+                    <span className="capitalize ">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
